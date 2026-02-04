7d49a9d17f43c6ad478f844c370902f5b03683c9
emal0n <edmundoneto511@hotmail.com>
2026-02-04 16:32:47 -0300

    regen script links

--- setup.sh
+++ setup.sh
@@ -3,7 +3,7 @@
 set -e
 
 DOTFILES_DIR="$HOME/.local/share/dotfiles"
-REPO_URL="https://github.com/vimanoelkkj/dotfiles.git"
+REPO_URL="https://github.com/emal0n/dotfiles.git"
 
 echo "=============="
 echo "Dotfiles Setup"
